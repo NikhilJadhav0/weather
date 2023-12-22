4bc7f32b621736f21d398d34aea086e7bfc7b167
NIKHIL JADHAV <128183802+NikhilJadhav0@users.noreply.github.com>
2023-12-22 20:37:08 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,52 +1,30 @@
-let weather = {
-        apiKey: "611ff4e00eefa5fcd99c5de20b7b0524",
-        fetchWeather: function (city) {
-          fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=" +
-              city +
-              "&units=metric&appid=" +
-              this.apiKey
-          )
-            .then((response) => {
-              if (!response.ok) {
-                alert("No weather found.");
-                throw new Error("No weather found.");
-              }
-              return response.json();
-            })
-            .then((data) => this.displayWeather(data));
-        },
-        displayWeather: function (data) {
-          const { name } = data;
-          const { icon, description } = data.weather[0];
-          const { temp, humidity } = data.main;
-          const { speed } = data.wind;
-          document.querySelector(".city").innerText = "Weather in " + name;
-          document.querySelector(".icon").src =
-            "https://openweathermap.org/img/wn/" + icon + ".png";
-          document.querySelector(".description").innerText = description;
-          document.querySelector(".temp").innerText = temp + "°C";
-          document.querySelector(".humidity").innerText ="Humidity: " + humidity + "%";
-          document.querySelector(".wind").innerText ="Wind speed: " + speed + " km/h";
-          document.querySelector(".weather").classList.remove("loading");
-          document.body.style.backgroundImage =
-            "url('https://source.unsplash.com/1600x900/?" + name + "')";
-        },
-        search: function () {
-          this.fetchWeather(document.querySelector(".search-bar").value);
-        },
-      };
-      
-//       document.querySelector(".search button").addEventListener("click", function () {
-//         weather.search();
-//       });
-      
-      document
-        .querySelector(".search-bar")
-        // .addEventListener("keyup", function (event) {
-        //   if (event.key == "Enter") {
-        //     weather.search();
-        //   }
-        // });
-      
-      weather.fetchWeather("pune");
+const API_KEY = `611ff4e00eefa5fcd99c5de20b7b0524`
+const from = document.querySelector("form")
+const search = document.querySelector("#search")
+const getWeather = async (city) => {
+	weather.innerHTML = `<h2> Loading.... <h2>`
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+	const response = await fetch(url);
+	const data = await response.json()
+	return showWeather(data)
+}
+const showWeather = (data) =>{
+	if(data){
+		weather.innerHTML = `<h2> City Not Found <h2>`
+	}
+	weather.innerHTML = `
+	<div>
+                <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
+            </div>
+            <div>
+                <h2>${data.main.temp} ℃</h2>
+                <h4> ${data.weather[0].main} </h4>
+            </div> `
+}
+from.addEventListener(
+	"submit", 
+	function(event){
+		getWeather(search.value)
+		event.preventDefault();
+	}
+)
